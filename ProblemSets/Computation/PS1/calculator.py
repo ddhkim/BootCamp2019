8f3c74c309c6e0341df6dcb9407c39496816e636
ddhkim <dohankim@sas.upenn.edu>
2019-07-07 23:47:14 -0500

    update

---
+++ ProblemSets/Computation/PS1/calculator.py
@@ -0,0 +1,13 @@
+"""This script is for Problem 3 in StandardLibrary
+   which contains:
+   a function that returns the sum of two arguments
+   a function that returns the product of two arguments
+"""
+
+
+def sum(x, y):
+    return x + y
+
+
+def product(x, y):
+    return x * y
